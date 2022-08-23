296cce0462c8afe7c17422ac101fdf6bc3e323e2
Fernando <58951154+fernandotmoreira@users.noreply.github.com>
2022-08-23 18:15:06 -0300

    first

---
+++ video_Bonus.js
@@ -0,0 +1,20 @@
+const { chromium } = require('playwright');
+const { expect } = require('expect');
+
+(async () => {
+    const browser = await chromium.launch()
+    const context = await browser.newContext({ recordVideo: { dir: './recordings' } });
+    const page = await context.newPage()
+
+    await page.goto('https://react-redux.realworld.io/#/login')
+
+    await page.fill('input[type = "email"]', 'fernandotoledomoreira@gmail.com')
+    await page.press('input[type = "email"]', 'Tab')
+    await page.type('input[type = "password"]', 'test123')
+    await page.click("form >> 'Sign in'")
+    
+    const html = await page.innerHTML('.feed-toggle')
+    expect(html).toMatch('Your Feed')
+
+    await browser.close()
+})()
